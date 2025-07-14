8d0144c0071caf75ccab41585aed2501348c0924
fcvq <lazy.fnm@gmail.com>
2025-07-14 01:46:19 -0400

    Update server.py

--- server.py
+++ server.py
@@ -43,5 +43,6 @@ def view_logs_page():
     return render_template('logs.html')
 
 if __name__ == '__main__':
+    import os
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
